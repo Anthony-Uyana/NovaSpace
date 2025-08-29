d1a929e424d49477f0498ee6e514ead05f897224
Anthony Gabriel Uyana Pulupa <antonythegame@hotmail.com>
2025-08-29 23:24:25 +0200

    Pipeline test

--- NovaSpace.API/Program.cs
+++ NovaSpace.API/Program.cs
@@ -20,7 +20,7 @@ namespace NovaSpace.API
             {
                 app.MapOpenApi();
             }
-
+            //Test Comment
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
