3ce5ddc4021d928aefb06ac212c362cdae73a084
Anthony Gabriel Uyana Pulupa <antonythegame@hotmail.com>
2025-08-29 23:41:16 +0200

    Fix

--- NovaSpace.API/Program.cs
+++ NovaSpace.API/Program.cs
@@ -20,7 +20,6 @@ namespace NovaSpace.API
             {
                 app.MapOpenApi();
             }
-            //Test Comment
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
