82dc7261ccb00e547446f50bd151a44dbdcb4cce
Anthony Gabriel Uyana Pulupa <antonythegame@hotmail.com>
2025-08-30 18:05:11 +0200

    Refactor y mejora de la estructura del proyecto
    Se simplifica el código en `WeatherForecastController.cs` y `WeatherForecast.cs`, eliminando llaves innecesarias y utilizando expresiones de cuerpo. Se actualiza la clase `Program` en `Program.cs` para mejorar la legibilidad y se añaden impresiones en consola.
    
    Se realizan cambios en `docker-compose.yml` para añadir un servicio API y ajustar configuraciones de puertos y variables de entorno. Se incluye un `PULL_REQUEST_TEMPLATE.md` para facilitar la documentación de cambios.
    
    Los `Dockerfile` se actualizan a .NET 10 Preview, mejorando la construcción y publicación de las aplicaciones API y Client. Se crea un archivo `nginx.conf` para configurar un servidor Nginx que sirva la aplicación Blazor WebAssembly.

--- NovaSpace.Client/Program.cs
+++ NovaSpace.Client/Program.cs
@@ -1,19 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace NovaSpace.Client
+namespace NovaSpace.Client;
+
+public class Program
 {
-    public class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+        WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        _ = builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
+        await builder.Build().RunAsync();
     }
 }
